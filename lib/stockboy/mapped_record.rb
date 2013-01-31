c40cf2def2845999f91b0176447c251b1bf64093
Andrew Vit <andrew@avit.ca>
2013-01-30 17:53:13 -0800

    Share metaprogrammed accessors between instances

--- lib/stockboy/mapped_record.rb
+++ lib/stockboy/mapped_record.rb
@@ -1,15 +1,33 @@
 module Stockboy
   class MappedRecord
-    def initialize(fields)
-      @fields = fields
-      @fields.keys.each do |k|
-        instance_eval "def #{k}; @fields[:#{k}] end"
+
+    module AccessorMethods
+      def self.for(attrs)
+        @module_registry        ||= Hash.new
+        @module_registry[attrs] ||= build_module(attrs)
       end
+
+      def self.build_module(attr_accessor_keys)
+        Module.new do
+          attr_accessor_keys.each do |key|
+            define_method key do
+              @fields[key]
+            end
+            # module_eval "def #{key}; @fields[:#{key}] end"
+          end
+        end
+      end
+    end
+
+    def initialize(fields)
+      mod = AccessorMethods.for(fields.keys)
+      extend mod
+      @fields = fields
       freeze
     end
 
     def to_s
-      @fields
+      @fields.to_s
     end
   end
 end
--- spec/stockboy/mapped_record_spec.rb
+++ spec/stockboy/mapped_record_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+require 'stockboy/mapped_record'
 
 module Stockboy
   describe MappedRecord do
@@ -8,6 +9,22 @@ module Stockboy
 
     it "accesses initialized fields from hash" do
       record.full_name.should == 'Arthur Dent'
+    end
+
+    it "does not redefine accessor methods" do
+      record1 = MappedRecord.new(:full_name => 'Arthur Dent')
+      record2 = MappedRecord.new(:full_name => 'Arthur Dent')
+
+      record1.method(:full_name).unbind.should ==
+      record2.method(:full_name).unbind
+    end
+
+    it "only has its own accessor methods" do
+      record1 = MappedRecord.new(:first_name => 'Arthur')
+      record2 = MappedRecord.new(:last_name => 'Dent')
+
+      record1.should_not respond_to(:last_name)
+      record2.should_not respond_to(:first_name)
     end
   end
 end
