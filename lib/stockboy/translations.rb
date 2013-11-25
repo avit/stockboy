96ff7ea7a368bdc3f17894899e412c24cdd8982c
Andrew Vit <andrew@avit.ca>
2013-11-25 12:23:10 -0800

    Clean up code out of class << self
    Make translations more in line with other Registry modules.

--- lib/stockboy/translations.rb
+++ lib/stockboy/translations.rb
@@ -4,35 +4,35 @@ require 'stockboy/translator'
 module Stockboy
   module Translations
 
-    @translators ||= {}
+    @registry ||= {}
 
+    def self.register(name, callable)
+      if callable.respond_to?(:call) or callable < Stockboy::Translator
+        @registry[name.to_sym] = callable
+      else
+        raise ArgumentError, "Registered translators must be callable"
+      end
+    end
+
+    def self.translate(func_name, context)
+      translator_for(:value, func_name).call(context)
+    end
+
+    def self.translator_for(attr, lookup)
+      if lookup.respond_to?(:call)
+        lookup
+      elsif tr = self[lookup]
+        tr.is_a?(Class) && tr < Stockboy::Translator ? tr.new(attr) : tr
+      else
+        ->(context) { context.public_send attr } # no-op
+      end
+    end
+
+    def self.find(func_name)
+      @registry[func_name]
+    end
     class << self
-      def register(name, callable)
-        if callable.respond_to?(:call) or callable < Stockboy::Translator
-          @translators[name.to_sym] = callable
-        else
-          raise ArgumentError, "Registered translators must be callable"
-        end
-      end
-
-      def translate(func_name, context)
-        translator_for(:value, func_name).call(context)
-      end
-
-      def find(func_name)
-        @translators[func_name]
-      end
       alias_method :[], :find
-
-      def translator_for(attr, lookup)
-        if lookup.respond_to?(:call)
-          lookup
-        elsif tr = self[lookup]
-          tr.is_a?(Class) && tr < Stockboy::Translator ? tr.new(attr) : tr
-        else
-          ->(context) { context.public_send attr } # no-op
-        end
-      end
     end
 
   end
