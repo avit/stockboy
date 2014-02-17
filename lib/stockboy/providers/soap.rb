6f71797bf93dc040638ce54df062fb07979c1a85
Mark Edmondson <mark@guestfolio.com>
2014-02-17 13:37:45 -0800

    Add more configurables to soap provider

--- lib/stockboy/providers/soap.rb
+++ lib/stockboy/providers/soap.rb
@@ -34,11 +34,23 @@ module Stockboy::Providers
     #
     dsl_attr :request
 
+    # @return [Symbol]
+    # @example
+    #   soap_action :soapenv
+    #
+    dsl_attr :env_namespace
+
     # @return [String]
     # @!attribute [rw] namespace
     #   Optional if specified in WSDL
     #
     dsl_attr :namespace
+
+    # @return [Hash]
+    # @!attribute [rw] namespaces
+    #   Optional if specified in WSDL
+    #
+    dsl_attr :namespaces
 
     # @return [String]
     # @!attribute [rw] namespace_id
@@ -62,6 +74,45 @@ module Stockboy::Providers
     #
     dsl_attr :message
 
+    # Message tag name
+    #
+    # @!attribute [rw] message_tag
+    # @return [String]
+    # @example
+    #   message_tag "GetResult"
+    #
+    dsl_attr :message_tag
+
+    # @return [String]
+    # @example
+    #   soap_action "urn:processMessage"
+    #
+    dsl_attr :soap_action
+
+    # XML string to override the Soap headers
+    #
+    # @!attribute [rw] soap_header
+    # @return [String]
+    # @example
+    #   soap_action "<soapenv:Header xmlns:wsa="http://www.w3.org/2005/08/addressing">..."
+    #
+    dsl_attr :soap_header
+
+    # @return [Hash]
+    # @example
+    #   attributes {}
+    #
+    dsl_attr :attributes
+
+    # Change the default response type, default :hash
+    #
+    # @!attribute [rw] response_format
+    # @return [Symbol]
+    # @example
+    #   response_format :xml
+    #
+    dsl_attr :response_format
+
     # Hash of optional HTTP request headers
     #
     # @!attribute [rw] headers
@@ -71,12 +122,22 @@ module Stockboy::Providers
     #
     dsl_attr :headers
 
+    # Array of WSSE Auth values
+    #
+    # @!attribute [rw] wsse_auth
+    # @return [Array]
+    # @example
+    #   wsse_auth ["Username", "Password"]
+    #
+    dsl_attr :wsse_auth
+
     # @!endgroup
 
     # Initialize a new SOAP provider
     #
     def initialize(opts={}, &block)
       super
+      @response_format = opts[:response_format] || :hash
       DSL.new(self).instance_eval(&block) if block_given?
     end
 
@@ -100,8 +161,11 @@ module Stockboy::Providers
       end
       opts[:convert_response_tags_to] = ->(tag) { string_pool(tag) }
       opts[:namespace] = namespace if namespace
+      opts[:namespaces] = namespaces if namespaces
       opts[:namespace_identifier] = namespace_id if namespace_id
+      opts[:env_namespace] = env_namespace if env_namespace
       opts[:headers] = headers if headers
+      opts[:wsse_auth] = wsse_auth if wsse_auth
       opts
     end
 
@@ -112,9 +176,16 @@ module Stockboy::Providers
 
     def fetch_data
       with_string_pool do
-        @data = client.call(@request, message: message).body
+        response = client.call(
+          @request,
+          message:         message,
+          message_tag:     message_tag,
+          soap_action:     soap_action,
+          soap_header:     soap_header,
+          attributes:      attributes
+        )
+        @data = response.send(response_format)
       end
     end
-
   end
 end
